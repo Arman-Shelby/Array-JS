4d9ba2093fb79a8be26184b4c5834f5611744c3c
Arman Mir <mirarman8583@gmail.com>
2025-07-31 15:29:03 +0600

    Shift-Unshift

---
+++ shift-unshift.js
@@ -0,0 +1,10 @@
+const friends = ['Balam', 'Kalam', 'Salam',  'Gelam' , 'Pailam'];
+
+console.log(friends);
+
+
+friends.shift();
+console.log(friends);
+
+friends.unshift('Ghumailam');
+console.log(friends);
