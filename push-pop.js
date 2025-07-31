7fd76d9078273c96ab372a785216c370053fae32
Arman Mir <mirarman8583@gmail.com>
2025-07-31 15:25:09 +0600

    Push-Pop

---
+++ push-pop.js
@@ -0,0 +1,24 @@
+const numbers = [12, 234, 443, 33, 333, 2322, 333, 2334, 234, 443, 33, 333, 2322, 333, 2334];
+
+const ages = [];
+
+
+//Push Arrays
+console.log(numbers);
+numbers.push(44);
+numbers.push(96);
+numbers.push(11, 9);
+console.log(numbers);
+
+const friends = ['Balam', 'Kalam', 'Salam',  'Gelam' , 'Pailam'];
+// friends.push('Khailam');
+console.log(friends);
+
+const out1 = friends.pop();
+console.log(friends);
+const out2 = friends.pop();
+console.log(friends);
+const out3 = friends.pop();
+console.log(friends);
+console.log(out1, out2);
+//Push - Pop --> array er last element e hoy
