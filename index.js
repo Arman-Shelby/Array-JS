b3505073ac77aa9407518e3ce1edcd7c67ffa2bc
Arman Mir <mirarman8583@gmail.com>
2025-07-31 14:57:10 +0600

    array index and length

---
+++ index.js
@@ -0,0 +1,20 @@
+const numbers = [12, 234, 443, 33, 333, 2322, 333, 2334, 234, 443, 33, 333, 2322, 333, 2334];
+
+console.log(numbers);
+console.log(numbers[4]);
+console.log(numbers[1]);
+console.log(numbers[ 8]);
+console.log(numbers[7]);
+
+
+
+const fourth = numbers[3];
+console.log(fourth);
+
+//set or update value ny index
+let abc = 45;
+abc = 96;
+
+
+numbers[1] = 999;
+console.log(numbers);
