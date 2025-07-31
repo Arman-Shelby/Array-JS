b124ebcffa02fc842c09f3b8a3548246b0d96820
Arman Mir <mirarman8583@gmail.com>
2025-07-31 07:57:51 +0600

    Array

---
+++ array.js
@@ -0,0 +1,4 @@
+const numbers = [14 , 45, 467, 474, 446, 464 ,4464 ,4646];
+const friends = ['Abu', 'Babul', 'Kabul', 'Bulbul'];
+const passed = [true, false , true , false , true];
+const mixedArray = [12, 'Arman', true, 45.689];
